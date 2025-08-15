030a62f364ba293a4797900ac4057637d890aaca
Josh Paul <josh4044@gmail.com>
2025-08-14 23:19:46 -0400

    namespace Rails to API

--- rails-api-devise-jwt-template/my_api/config/routes.rb
+++ rails-api-devise-jwt-template/my_api/config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users,
-    path: "auth",
-    path_names: {
-      sign_in: "sign_in",
-      sign_out: "sign_out",
-      registration: "sign_up"
-    },
-    controllers: {
-      sessions: "auth/sessions",
-      registrations: "auth/registrations"
-    }
+  namespace :api do
+    devise_for :users,
+      path: "auth",
+      path_names: {
+        sign_in: "sign_in",
+        sign_out: "sign_out",
+        registration: "sign_up"
+      },
+      controllers: {
+        sessions: "auth/sessions",
+        registrations: "auth/registrations"
+      }
 
-  get "/health", to: proc { [200, { "Content-Type" => "application/json" }, [ { ok: true }.to_json ]] }
-  get "/me", to: "me#show"
+    get "/health", to: proc { [200, { "Content-Type" => "application/json" }, [ { ok: true }.to_json ]] }
+    get "/me", to: "me#show"
+  end
 end
