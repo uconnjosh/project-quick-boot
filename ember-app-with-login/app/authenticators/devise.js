6f82a7caf8d752517fd1567544b3a2c0f271d142
Josh Paul <josh4044@gmail.com>
2025-08-14 22:24:34 -0400

    back to API

--- ember-app-with-login/app/authenticators/devise.js
+++ ember-app-with-login/app/authenticators/devise.js
@@ -2,5 +2,5 @@ import DeviseAuthenticator from 'ember-simple-auth/authenticators/devise';
 
 // todo fix the name
 export default class OAuth2Authenticator extends DeviseAuthenticator {
-  serverTokenEndpoint = "auth/sign_in";
+  serverTokenEndpoint = "api/auth/sign_in";
 }
